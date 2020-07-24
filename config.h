307ff5b2a60462ec8f1d9c71677c69a8beee7718
skuzzymiglet <skuzzymiglet@gmail.com>
2020-07-24 16:15:04 +0100

    Bye rounded corners, too buggy

--- config.h
+++ config.h
@@ -15,7 +15,6 @@ static const char *colors[][3]      = {
 	[SchemeSel]  = { dark0, light1 , bright_orange  },
 };
 
-static const int CORNER_RADIUS = 30;
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
